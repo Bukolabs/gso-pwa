e25a16837a9f846a1b879e642a833572f297a5c5
Jed Achas <jedachas@gmail.com>
2023-10-30 09:04:15 +0000

    rename files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./app.css";
 
 function App() {
+  const [source, setSource] = useState("");
+
+  const handleCapture = (target: any) => {
+    if (target.files) {
+      if (target.files.length !== 0) {
+        const file = target.files[0];
+        const newUrl = URL.createObjectURL(file);
+        setSource(newUrl);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to GSO Super App</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Track
-        </a>
+
+        {source && (
+          <div>
+            <img src={source} alt={"snap"}></img>
+          </div>
+        )}
+
+        <input
+          accept="image/*"
+          id="icon-button-file"
+          type="file"
+          capture="environment"
+          onChange={(e) => handleCapture(e.target)}
+        />
+        <label htmlFor="icon-button-file">
+          <button color="primary" aria-label="upload picture">
+            Snap
+          </button>
+        </label>
       </header>
     </div>
   );
