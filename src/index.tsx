8c4dd23289ecab231f15ca38d91f87785d6f0bca
Jed Achas <jedachas@gmail.com>
2023-11-05 07:44:13 +0000

    restructure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.scss";
-import App from "./app";
+import App from "./app/app";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
