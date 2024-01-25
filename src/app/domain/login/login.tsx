6b7abf0b5c15208ef441c55358afb1f1dbf6d17f
Jed Achas <jedachas@gmail.com>
2024-01-25 18:11:52 +0000

    update version

--- src/app/domain/login/login.tsx
+++ src/app/domain/login/login.tsx
@@ -115,7 +115,7 @@ export function Login() {
           className="flex flex-col items-center justify-center"
           onClick={handleBukoLogo}
         >
-          <p>v240125.1</p>
+          <p>v240125.2</p>
           <p className="m-0 font-bold">Powered by:</p>
           <img
             src={bukoLogo}
