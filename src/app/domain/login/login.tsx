6b4591dd1a78e1df229e3840a7402d8a0c75688b
Jed Achas <jedachas@gmail.com>
2024-02-04 11:42:07 +0000

    inc build

--- src/app/domain/login/login.tsx
+++ src/app/domain/login/login.tsx
@@ -122,7 +122,7 @@ export function Login() {
             alt="buko-labs"
           />
         </div>
-        <span className="text-center fixed bottom-0 right-0 text-gray-300">v240402.4</span>
+        <span className="text-center fixed bottom-0 right-0 text-gray-300">v240402.5</span>
       </LeftContentPage>
     </div>
   );
