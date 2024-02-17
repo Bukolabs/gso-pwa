8053553370c47b6ba6e3fc922946b46a1be174f4
Jed Achas <jedachas@gmail.com>
2024-02-17 11:54:42 +0000

    feat: assign item forms in bulk

--- src/app/domain/purchase-request/item-bulk-uploader/item-bulk-uploader.tsx
+++ src/app/domain/purchase-request/item-bulk-uploader/item-bulk-uploader.tsx
@@ -3,6 +3,9 @@ import "./item-bulk-uploader.scss";
 import Papa from "papaparse";
 import { useNotificationContext } from "@shared/ui/notification/notification.context";
 import { useQyGetCategory } from "@core/query/category.query";
+import { useGetUnit } from "@core/query/unit.query";
+import { PurchaseItemFormSchema } from "@core/model/form.rule";
+import { useGetItem } from "@core/query/item.query";
 
 /* eslint-disable-next-line */
 export interface ItemBulkUploaderProps {}
@@ -11,7 +14,9 @@ export function ItemBulkUploader() {
   const { showInfo, showWarning } = useNotificationContext();
 
   // API GET CATEGORY
-  useQyGetCategory();
+  const { data: categories } = useQyGetCategory();
+  const { data: units } = useGetUnit("", 999999, 0);
+  const { data: items } = useGetItem('', 99999999999, 0)
 
   // API GET UNIT
 
@@ -37,10 +42,21 @@ export function ItemBulkUploader() {
     event.options.clear();
   };
   const validateSheetData = (data: string[][]) => {
+    const categoriesData = categories?.data || [];
+    const unitsData = units?.data || [];
+    const itemsData = items?.data || [];
     const headers = data[0];
+    const content = data.slice(1);
 
+    if (categoriesData.length === 0 || unitsData.length === 0) {
+      showWarning(
+        "Unable to retrieve categories and units from server. Please refresh the page."
+      );
+      return;
+    }
+
+    // Get Header
     const correctHeaders = [
-      "brand",
       "category",
       "unit",
       "name",
@@ -60,7 +76,54 @@ export function ItemBulkUploader() {
       return;
     }
 
-    console.log({ data, headers, hasCorrectHeaders });
+    // Get Content
+    const itemFormContents = content.map((row) => {
+      const filteredCategory = categoriesData.filter((x) => x.name === row[0]);
+      const filteredUnit = unitsData.filter((x) => x.name === row[1]);
+      const filteredItem = itemsData.filter(x => x.name === row[2])
+
+      const categoryCode =
+        filteredCategory.length > 0 ? filteredCategory[0].code : "";
+      const categoryName = row[0];
+
+      const unitCode = filteredUnit.length > 0 ? filteredUnit[0].code : "";
+      const unitName = row[1];
+
+      const nameCode = filteredItem.length > 0 ? filteredItem[0].code : undefined;      
+      const name = row[2];
+
+      const cost = parseInt(row[3], 10);
+      const quantity = parseInt(row[4], 10);
+      const description = row[5];
+
+      const itemForm = {
+        itemArrayCode: undefined,
+        code: nameCode,
+        name,
+        description,
+        unit: unitCode,
+        unitName,
+        category: categoryCode,
+        categoryName,
+        brand: "",
+        brandName: "",
+        cost,
+        isActive: true,
+        quantity,
+        deliveredQuantity: 0,
+      } as PurchaseItemFormSchema;
+
+      return itemForm;
+    });
+
+    console.log({
+      data,
+      headers,
+      hasCorrectHeaders,
+      categories,
+      content,
+      itemFormContents,
+    });
   };
 
   return (
