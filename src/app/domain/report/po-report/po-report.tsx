aa0aef39513aa1dabb76667845c8a1255090da45
codestitch <jedachas@gmail.com>
2024-03-12 10:55:27 +0000

    feat: add cards in report

--- src/app/domain/report/po-report/po-report.tsx
+++ src/app/domain/report/po-report/po-report.tsx
@@ -8,86 +8,20 @@ import colors from "tailwindcss/colors";
 import SkeletonList from "@shared/ui/skeleton-list/skeleton-list";
 
 export interface PoReportProps {
+  isLoading: boolean;
+  chartData: any;
+  chartOptions: any;
   dateName?: string;
   startDate?: string;
   endDate?: string;
-  onSelected: (label: string) => void;
+  onSelected?: (label: string) => void;
 }
 
 export function PoReport({
-  dateName,
-  startDate,
-  endDate,
-  onSelected,
+  isLoading,
+  chartData,
+  chartOptions,
 }: PoReportProps) {
-  const [chartData, setChartData] = useState({});
-  const [chartOptions, setChartOptions] = useState({});
-
-  const handleSuccessApi = (
-    response: DashboardControllerPrDashboardReport200Response
-  ) => {
-    const prData = response.data;
-
-    const dataRawLabels = (prData || []).map((item) => item.label);
-    const dataLabels = (prData || []).map(
-      (item) => reportLabels[item.label as keyof typeof reportLabels]
-    );
-    const dataValues = (prData || []).map((item) => item.value);
-    const data = {
-      labels: dataLabels,
-      datasets: [
-        {
-          data: dataValues,
-          backgroundColor: [
-            colors.blue[500],
-            colors.cyan[500],
-            colors.violet[500],
-            colors.teal[500],
-            colors.red[500],
-            colors.lime[500],
-            colors.pink[500],
-            colors.emerald[500],
-          ],
-          hoverBackgroundColor: [
-            colors.blue[400],
-            colors.cyan[400],
-            colors.violet[400],
-            colors.teal[400],
-            colors.red[400],
-            colors.lime[400],
-            colors.pink[400],
-            colors.emerald[400],
-          ],
-        },
-      ],
-    };
-    const options = {
-      plugins: {
-        legend: {
-          labels: {
-            usePointStyle: true,
-          },
-        },
-      },
-      onClick: (_: any, item: any) => {
-        if (item.length > 0) {
-          const selectedIndex = item[0].index;
-          onSelected(dataRawLabels[selectedIndex]);
-        }
-      },
-    };
-
-    setChartData(data);
-    setChartOptions(options);
-  };
-  const { isLoading } = useGetQyOrderReport(
-    undefined,
-    dateName,
-    startDate,
-    endDate,
-    handleSuccessApi
-  );
-
   const displayLoading = (
     <div className="card">
       <SkeletonList count={1} mode="dashboard" />
